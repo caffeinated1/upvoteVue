3b51904c0c753d008c2ae1eef7290cd276cbf4ea
David Espana <davidespana@davids-mbp.attlocal.net>
2020-05-10 20:47:23 -0400

    revised working files

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-//Vue MAGIC
-new Vue({
-el: '#app',
-data :{
-  submissions: seed.submissions
-}
+// adding vue
+new Vue ({
+  el: '#app',
+  data : {
+    submissions : Seed.submissions
+  },
+  computed: {
+      sortedSubmissions(){
+        return this.submissions.sort((a ,b) => {
+          return b.votes - a.votes
+        });
+      }
+    },
+
+  methods:  {
+    upvote(submissionId){
+      const submission = this.submissions.find(
+        submission => submission.id === submissionId
+      );
+      submission.votes++;
+    }
+  }
+
 });
